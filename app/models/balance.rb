0cc10ba2a561e7812342ea1927093c094d8c6db4
Vitaly A. Sorokin <erundook@gmail.com>
2013-11-12 22:12:01 +0400

    balance audit

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -9,11 +9,12 @@ class Balance < ActiveRecord::Base
 
   scope :currency, -> id { where(currency_id: id) }
 
-  def add_funds(amount, subject)
+  def add_funds(amount, subject, comment = nil)
     self.with_lock do
       increment :amount, amount
       balance_changes.create(
         subject:  subject,
+        comment:  comment || "add_funds",
         amount:   amount
       ) if save
     end
@@ -21,10 +22,12 @@ class Balance < ActiveRecord::Base
 
   def lock_funds(lock_amount, subject)
     self.with_lock do
+      self.verify!
       return false if self.amount < lock_amount
-      decrement :amount, lock_amount
+      decrement :amount,  lock_amount
       increment :held,    lock_amount
       balance_changes.create(
+        comment: "lock_funds",
         subject:  subject,
         amount:  -lock_amount,
         held:     lock_amount
@@ -34,12 +37,14 @@ class Balance < ActiveRecord::Base
 
   def unlock_funds(unlock_amount, subject, move = true)
     self.with_lock do
+      self.verify!
       return false if self.held < unlock_amount
-      increment :amount, unlock_amount if move
+      increment :amount,  unlock_amount if move
       decrement :held,    unlock_amount
       balance_changes.create(
+        comment: "unlock_funds(move=#{move})",
         subject:  subject,
-        amount:   unlock_amount,
+        amount:   move ? unlock_amount : 0,
         held:    -unlock_amount
       ) if save
     end
@@ -60,4 +65,26 @@ class Balance < ActiveRecord::Base
       BalanceSerializer.new(self, root: false))
   end
 
+  def verify(detailed = false)
+    v = balance_changes.select('sum(amount) as amount, sum(held) as held').first
+    v.held ||= 0
+    v.amount ||= 0
+    result = v.amount == self.amount && v.held == self.held
+    if detailed
+      return {
+        held:        v.held,
+        valid:       result,
+        amount:      v.amount,
+        held_diff:   self.held - v.held,
+        amount_diff: self.amount - v.amount
+      }
+    else
+      return result
+    end
+  end
+
+  def verify!
+    v = self.verify(true)
+    update_attributes(amount: v[:amount], held: v[:held]) unless v[:valid]
+  end
 end
