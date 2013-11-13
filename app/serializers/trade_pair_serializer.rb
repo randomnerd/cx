7aec6037c1c88356ef1b7d407aa8d85ca442330e
Vitaly A. Sorokin <erundook@gmail.com>
2013-11-13 21:13:20 +0400

    add updated_at to trade pairs

--- app/serializers/trade_pair_serializer.rb
+++ app/serializers/trade_pair_serializer.rb
@@ -1,5 +1,5 @@
 class TradePairSerializer < ActiveModel::Serializer
   attributes :id, :buy_fee, :sell_fee, :last_price, :currency_id, :market_id,
              :public, :url_slug, :rate_min, :rate_max, :currency_volume,
-             :market_volume
+             :market_volume, :updated_at
 end
