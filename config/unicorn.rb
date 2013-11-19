5ca8bed0b1a18b199231432e6a4be689ed15f281
Vitaly A. Sorokin <erundook@gmail.com>
2013-11-19 23:21:45 +0400

    remove EM fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,18 +16,6 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 
-  # eventmachine fix
-  if defined?(EventMachine)
-    unless EventMachine.reactor_running? && EventMachine.reactor_thread.alive?
-      if EventMachine.reactor_running?
-        EventMachine.stop_event_loop
-        EventMachine.release_machine
-        EventMachine.instance_variable_set("@reactor_running",false)
-      end
-      Thread.new { EventMachine.run }
-    end
-  end
-
   Signal.trap("INT") { EventMachine.stop }
   Signal.trap("TERM") { EventMachine.stop }
 end
