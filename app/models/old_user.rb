1900792a08bd0ca5a6ba6ef182c362283fdfe2db
Vitaly A. Sorokin <erundook@gmail.com>
2013-12-11 21:56:53 +0400

    migrate workers and deposit addresses

--- app/models/old_user.rb
+++ app/models/old_user.rb
@@ -8,6 +8,7 @@ class OldUser
 
   def self.migrate
     OldUser.all.no_timeout.each do |user|
+      next if User.find_by_email(user.emails.first['address'])
       new_user = User.create({
         email: user.emails.first['address'],
         nickname: user.profile.try(:[], 'nickname'),
@@ -21,6 +22,15 @@ class OldUser
         next unless (b.balance > 0 || b.held > 0)
         cid = Currency.find_by_old_id(b.currId)
         new_user.balance_for(cid).add_funds(b.balance + b.held, nil, 'migrated balance')
+      end
+      OldWorker.where(userId: user._id).all.each do |w|
+        new_user.workers.create({name: w.name, pass: w.pass})
+      end
+      OldWallet.where(userId: user._id).all.each do |w|
+        new_user.wallets.create({
+          currency: Currency.find_by_old_id(w.currId),
+          address: w.addr
+        })
       end
     end
   end
