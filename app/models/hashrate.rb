4a187492a39b53edeb6fb49def43a756ef106c94
Vitaly A. Sorokin <erundook@gmail.com>
2013-12-12 02:54:29 +0400

    fix hashrates

--- app/models/hashrate.rb
+++ app/models/hashrate.rb
@@ -3,7 +3,7 @@ class Hashrate < ActiveRecord::Base
   belongs_to :currency
 
   scope :active, -> {
-    where('hashrates.updated_at > ?', 2.minutes.ago).limit(15)
+    where('hashrates.updated_at > ?', 2.minutes.ago).order('rate desc').limit(15)
   }
   scope :currency_name, -> name {
     joins(:currency).where(currencies: {name: name})
@@ -12,6 +12,11 @@ class Hashrate < ActiveRecord::Base
   include PusherSync
   def pusher_channel
     "hashrates-#{self.currency_id}"
+  end
+
+  def pusher_update
+    return unless self.class.active.include? self
+    PusherMsg.perform_async(pusher_channel, "u", pusher_serialize)
   end
 
   def self.set_rate(currency_id, user_id, hashrate)
