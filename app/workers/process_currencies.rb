4542f0036286d3008b691af3435daa9ce22d6d49
Vitaly A. Sorokin <erundook@gmail.com>
2013-12-14 05:55:55 +0400

    stop processing

--- app/workers/process_currencies.rb
+++ app/workers/process_currencies.rb
@@ -3,9 +3,9 @@ class ProcessCurrencies
 
   def perform
     Currency.all.each do |currency|
-      ProcessCurrency.perform_async(currency.id)
+      # ProcessCurrency.perform_async(currency.id)
       if currency.mining_enabled
-        ProcessPool.perform_async(currency.id)
+        # ProcessPool.perform_async(currency.id)
       end
     end
   end
