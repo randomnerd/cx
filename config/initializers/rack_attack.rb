0f636867939a0919a4d84552dd8f78c45d54e032
Vitaly A. Sorokin <erundook@gmail.com>
2014-01-13 11:12:02 +0400

    fix rack-attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,17 @@
-Rack::Attack.throttle('req/ip', :limit => 20, :period => 5.seconds) do |req|
+module Rack
+  class Request
+    def trusted_proxy?(ip)
+      ip == '95.211.38.220'
+    end
+  end
+end
+
+Rack::Attack.throttle('req/ip', :limit => 20, :period => 4.seconds) do |req|
   # If the return value is truthy, the cache key for the return value
   # is incremented and compared with the limit. In this case:
   #   "rack::attack:#{Time.now.to_i/1.second}:req/ip:#{req.ip}"
   #
   # If falsy, the cache key is neither incremented nor checked.
 
-  req.ip unless Rails.env.development?
-  false
+  Rails.env.development? ? false : req.ip
 end
