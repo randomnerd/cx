a622151006bf256020d799d4ae230cc4d1297bcc
Vitaly A. Sorokin <erundook@gmail.com>
2014-02-01 22:52:03 +0400

    fix messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140126181426) do
+ActiveRecord::Schema.define(version: 20140201184501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -206,7 +206,10 @@ ActiveRecord::Schema.define(version: 20140126181426) do
     t.boolean  "system"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
+
+  add_index "messages", ["created_at"], name: "index_messages_on_created_at", using: :btree
 
   create_table "notifications", force: true do |t|
     t.text     "body"
