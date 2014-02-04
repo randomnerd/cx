ef4d70d24f9f4ed43896c4db561a03e8aff2d4e9
Vitaly A. Sorokin <erundook@gmail.com>
2014-02-04 18:17:34 +0400

    fix changing nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,7 +39,7 @@ class User < ActiveRecord::Base
     name ||= email.split('@').first
     return if name == nickname
     name.gsub!(/[^\w]/, '')
-    name += rand(10).to_s while !!User.where('nickname like ?', name)
+    name += rand(10).to_s while !!User.where('nickname ilike ?', name).first
     return if name.empty?
     update_attribute :nickname, name
   end
