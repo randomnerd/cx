bb996150fa7a4ca88b495f8d13c64bc30507ac13
Vitaly A. Sorokin <erundook@gmail.com>
2014-02-08 16:23:37 +0400

    use balance_serializer for balances

--- app/controllers/api/v2/balances_controller.rb
+++ app/controllers/api/v2/balances_controller.rb
@@ -2,6 +2,10 @@ class Api::V2::BalancesController < Api::V2::BaseController
   before_filter :authenticate_user!, only: [:index]
   has_scope :currency
 
+  def index
+    render json: collection
+  end
+
   protected
   def begin_of_association_chain
     current_user
