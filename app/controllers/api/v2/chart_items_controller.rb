94af983c6e7264d7447ed94988a79ccfc1c1d1cc
Vitaly A. Sorokin <erundook@gmail.com>
2014-02-14 13:51:29 +0400

    order chart items by timestamp

--- app/controllers/api/v2/chart_items_controller.rb
+++ app/controllers/api/v2/chart_items_controller.rb
@@ -3,7 +3,7 @@ class Api::V2::ChartItemsController < Api::V2::BaseController
 
   def index
     rel = ChartItem.where(trade_pair_id: params[:trade_pair_id]).
-    where('time > ?', 1.month.ago)
+    where('time > ?', 1.month.ago).order(:time)
     render json: FastJson.raw_dump(rel)
   end
 end
